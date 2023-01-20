2feb76586209e6085f391e32736b6fa415b7d545
Андрей <parabellum.koval@gmail.com>
2023-01-20 21:25:52 +0200

    add trait methods

--- src/app/Traits/HasTransactions.php
+++ src/app/Traits/HasTransactions.php
@@ -3,8 +3,29 @@
 namespace Backpack\Transactions\app\Traits;
 
 trait HasTransactions {
+
   public function transactions() {
     return $this->hasMany('Backpack\Transactions\app\Models\Transaction', 'owner_id');
+  }
+
+  public function getBalanceByType($type) {
+    $sum = $this->transactions()->where('type', $type)->orderBy('id', 'DESC')->sum('value');
+    return $sum? $sum: 0;
+  }
+
+  public function getBalance() {
+    $last_transaction = $this->transactions()->orderBy('id', 'DESC')->first();
+    return $last_transaction->balance?  $last_transaction->balance: 0;
+  }
+
+  public function getDebit() {
+    $sum = $this->transactions()->where('value', '>', 0)->sum('value');
+    return $sum? $sum: 0;
+  }
+
+  public function getCredit() {
+    $sum = $this->transactions()->where('value', '<', 0)->sum('value');
+    return $sum? $sum: 0;
   }
 
   public function createTransaction($data, $target_model) {
